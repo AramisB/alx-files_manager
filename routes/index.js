4af1da58dc0d94bc7bc47dc1a56544590425cc6e
AramisB <brendachem7@gmail.com>
2024-09-28 18:39:52 +0300

    first API

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,34 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
+import { APIError, errorResponse } from '../middlewares/error';
 
-const router = express.Router();
-// GET /status => AppController.getStatus
-router.get('/status', AppController.getStatus);
-// GET /stats => AppController.getStats
-router.get('/stats', AppController.getStats);
+const injectRoutes = (app) => {
+  const api = express.Router();
+  app.use('/', api);
 
-// add a new endpoint: POST /users => UsersController.postNew
-router.post('/users', UsersController.postNew);
-// GET /connect => AuthController.getConnect
-router.get('/connect', AuthController.getConnect);
-// GET /disconnect => AuthController.getDisconnect
-router.get('/disconnect', AuthController.getDisconnect);
-// GET /users/me => UserController.getMe
-router.get('/users/me', UsersController.getMe);
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats);
 
-export default router;
+  api.get('/connect', basicAuthenticate, AuthController.getConnect);
+  api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+
+  api.post('/users', UsersController.postNew);
+  api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+
+  api.post('/files', xTokenAuthenticate, FilesController.postUpload);
+  api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
+  api.get('/files', xTokenAuthenticate, FilesController.getIndex);
+  api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
+  api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
+  api.get('/files/:id/data', FilesController.getFile);
+
+  api.all('*', (req, res, next) => {
+    errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
+  });
+  api.use(errorResponse);
+};
+
+export default injectRoutes;
