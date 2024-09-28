e26e699c11d530284cdb223a4c8d5f0d85557d22
AramisB <brendachem7@gmail.com>
2024-09-28 15:47:33 +0300

    export app

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-import express from "express"
-import routes from "./routes/index.js";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use("/", routes);
+app.use('/', routes);
 
 app.listen(PORT, () => {
-    console.log("Server started on port 5000");
+  console.log('Server started on port 5000');
 });
+
+export default app;
