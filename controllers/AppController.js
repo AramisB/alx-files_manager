54f568aa537d19dbc2e090a720b607dd47c4bda1
AramisB <brendachem7@gmail.com>
2024-09-28 16:14:49 +0300

    used static on getStatus() and getStats()

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,25 +1,21 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-const AppController = {
-  getStatus(req, res) {
-    const redisStatus = redisClient.isAlive();
-    const dbStatus = dbClient.isAlive();
-    res.status(200).json({ redis: redisStatus, db: dbStatus });
-  },
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
 
-  async getStats(req, res) {
-    try {
-      const users = await dbClient.nbUsers();
-      const files = await dbClient.nbFiles();
-      res.status(200).json({ users, files });
-    } catch (error) {
-      res.status(500).json({ error });
-      return res.status(500).json({ error: 'Internal server error' });
-    }
-
-    return null;
-  },
-};
-
-export default AppController;
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([nbUsers, nbFiles]) => {
+        res.status(200).json({
+          users: nbUsers,
+          files: nbFiles,
+        });
+      });
+  }
+}
